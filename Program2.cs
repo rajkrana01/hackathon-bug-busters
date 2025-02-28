47e6741cb616a334199e029bff459eda82282cdc
Rajesh Rana <rajkrana01@gmail.com>
2025-02-28 17:41:36 +0530

    BBH-16 bug fixes

---
+++ Program2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+class Program
+{
+    static void Main()
+    {
+        string localIP = GetLocalIPAddress();
+        Console.WriteLine($"Local IP Address: {localIP}");
+        Console.ReadLine();
+    }
+
+    static string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString(); // Return IPv4 address
+            }
+        }
+        throw new Exception("No network adapters with an IPv4 address in the system!");
+    }
+}
